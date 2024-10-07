0891b2aa2493511c01cf11bf31eada676172c86b
JoaoPedroDuarte <pepinho.jp@gmail.com>
2024-10-06 22:31:21 -0300

    Update e Delete de Atividade
    Co-Authored-By: JoaoDuarteCunha <171621708+JoaoDuarteCunha@users.noreply.github.com>

--- psiuApp/urls.py
+++ psiuApp/urls.py
@@ -6,6 +6,8 @@ app_name = "psiuApp"
 urlpatterns = [
     path('cria/', views.AtividadeCreateView.as_view(), name='cria-atividade'),
     path('listaAtividades/', views.AtividadeListView.as_view(), name='lista-atividades'), 
+    path('atualiza/<int:pk>/', views.AtividadeUpdateView.as_view(), name='atualiza-atividade'),
+    path('apaga/<int:pk>/', views.AtividadeDeleteView.as_view(), name='apaga-atividade'), 
     path('', views.home, name='homepage'), 
     path('segunda/', views.segundaPagina, name='segunda'),
 ] 
--- psiuApp/views.py
+++ psiuApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic.base import View 
 from psiuApp.forms import AtividadeModel2Form
@@ -27,11 +27,38 @@ class AtividadeCreateView(View):
     def get(self, request, *args, **kwargs): 
         contexto = { 'formulario': AtividadeModel2Form, } 
         return render(request, "psiuApp/criaAtividade.html", contexto) 
-
-
     def post(self, request, *args, **kwargs): 
         formulario = AtividadeModel2Form(request.POST) 
         if formulario.is_valid(): 
             contato = formulario.save() 
             contato.save() 
             return HttpResponseRedirect(reverse_lazy("psiuApp:lista-atividades")) 
+
+class AtividadeUpdateView(View): 
+    def get(self, request, pk, *args, **kwargs): 
+        atividade = Atividade.objects.get(pk=pk) 
+        formulario = AtividadeModel2Form(instance=atividade) 
+        context = {'atividade': formulario, } 
+        return render(request, 'psiuApp/atualizaAtividade.html', context) 
+     
+    def post(self, request, pk, *args, **kwargs): 
+        atividade = get_object_or_404(Atividade, pk=pk) 
+        formulario = AtividadeModel2Form(request.POST, instance=atividade) 
+        if formulario.is_valid(): 
+            atividade = formulario.save() # cria uma pessoa com os dados do formulário 
+            atividade.save()        # salva uma pessoa no banco de dados 
+            return HttpResponseRedirect(reverse_lazy("psiuApp:lista-atividades")) 
+        else: 
+            contexto = {'atividade': formulario, } 
+            return render(request, 'psiuApp/atualizaAtividade.html', contexto) 
+        
+class AtividadeDeleteView(View): 
+    def get(self, request, pk, *args, **kwargs): 
+        atividade = Atividade.objects.get(pk=pk) 
+        contexto = { 'atividade': atividade, } 
+        return render(request, 'psiuApp/apagaAtividade.html', contexto) 
+    
+    def post(self, request, pk, *args, **kwargs): 
+        atividade = Atividade.objects.get(pk=pk) 
+        atividade.delete() 
+        return HttpResponseRedirect(reverse_lazy("psiuApp:lista-atividades"))
